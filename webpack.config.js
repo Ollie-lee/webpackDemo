96e52d1748ae072e8590b3de9ed2cb1b5d949bb9
Ollie <myself.ollie.lee@gmail.com>
2020-09-29 15:11:33 +1000

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
 module.exports = {
-  mode:'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path:path.resolve(__dirname,'dist')
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // order matters!
+        //webpack loads loader of use array in reverse order
+        //i.e. here, css-loader is loaded first
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 }
