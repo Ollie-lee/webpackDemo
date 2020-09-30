e160b8b6168dd4d9e9f759da1ca92286e8bd2e12
Ollie <myself.ollie.lee@gmail.com>
2020-09-30 15:47:16 +1000

    minify css

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,14 @@
 const path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ],
+
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        // order matters!
-        //webpack loads loader of use array in reverse order
-        //i.e. here, css-loader is loaded first
-        use: [
-          "style-loader",//3. Inject styles into DOM
-          "css-loader",//2. Turns css into commonjs
-          'sass-loader'//1. Turns sass into css
-        ]
-      },
+
       {
         test: /\.html$/,
         use: ['html-loader']
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const path = require('path')
 const common = require('./webpack.common')
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,5 +13,41 @@ module.exports = merge(common, {
     filename: '[name].[contentHash].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contentHash].css'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // order matters!
+        //webpack loads loader of use array in reverse order
+        //i.e. here, css-loader is loaded first
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract css into files
+          "css-loader",//2. Turns css into commonjs
+          'sass-loader'//1. Turns sass into css
+        ]
+      },
+    ]
+  },
+  optimization: {
+    //we may minimize js,css and so on, so 
+    //minimizer is an array
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin()
+    ]
+  }
 })
